11832e029c62b7ca7aeac0f80f0e5afbd27c2d60
Romihia <118122586+Romihia@users.noreply.github.com>
2025-02-02 14:06:21 +0200

    update linear interpo

--- Interpolation and Polynomial Approximation/linear_interpolation.py
+++ Interpolation and Polynomial Approximation/linear_interpolation.py
@@ -1,4 +1,3 @@
-from colors import bcolors
 
 
 def linearInterpolation(table_points, point):
@@ -14,7 +13,7 @@ def linearInterpolation(table_points, point):
             y1 = table_points[i][1]
             y2 = table_points[i + 1][1]
             result = (((y1 - y2) / (x1 - x2)) * point) + ((y2 * x1) - (y1 * x2)) / (x1 - x2)
-            print(bcolors.OKGREEN, "\nThe approximation (interpolation) of the point ", point, " is: ",bcolors.ENDC, round(result, 4))
+            print("\nThe approximation (interpolation) of the point ", point, " is: ", round(result, 4))
             flag = 0
     if flag:
         x1 = table_points[0][0]
@@ -23,18 +22,18 @@ def linearInterpolation(table_points, point):
         y2 = table_points[1][1]
         m = (y1 - y2) / (x1 - x2)
         result = y1 + m * (point - x1)
-        print(bcolors.OKGREEN, "\nThe approximation (extrapolation) of the point ", point, " is: ",bcolors.ENDC, round(result, 4))
+        print("\nThe approximation (extrapolation) of the point ", point, " is: ", round(result, 4))
 
 
 if __name__ == '__main__':
 
-    table_points = [(0, 0), (1, 0.8415), (2, 0.9093), (3, 0.1411), (4, -0.7568), (5, -0.9589), (6, -0.2794)]
-    x = 1.28
-    print(bcolors.OKBLUE, "----------------- Interpolation & Extrapolation Methods -----------------\n", bcolors.ENDC)
-    print(bcolors.OKBLUE, "Table Points: ", bcolors.ENDC, table_points)
-    print(bcolors.OKBLUE, "Finding an approximation to the point: ", bcolors.ENDC, x)
+    table_points = [(6.5, 2.14451), (6.7, 2.35585), (7.0, 2.74748), (8.0, 5.67127)]
+    x = 6.9
+    print( "----------------- Interpolation & Extrapolation Methods -----------------\n")
+    print( "Table Points: ", table_points)
+    print( "Finding an approximation to the point: ",  x)
     linearInterpolation(table_points, x)
-    print(bcolors.OKBLUE, "\n---------------------------------------------------------------------------\n", bcolors.ENDC)
+    print( "\n---------------------------------------------------------------------------\n")
 
 
 
